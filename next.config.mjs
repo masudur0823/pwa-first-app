ea6c2e13405c19472b466c40846b58808a5a9f46
masudur0823 <masudur0823@gmail.com>
2024-08-04 22:04:34 +0600

    e

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const withPWA = withPWAInit({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
+  extendDefaultRuntimeCaching: true,
   // disable: false,
   workboxOptions: {
     // disableDevLogs: true,
