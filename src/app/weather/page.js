d9bff1233a7111e728de43f34dd05271bb10f6c5
masudur0823 <masudur0823@gmail.com>
2024-08-04 22:40:11 +0600

    edited

--- src/app/weather/page.js
+++ src/app/weather/page.js
@@ -1,5 +1,35 @@
-import React from "react";
+"use client";
 
-export default function Weather() {
-  return <div>Weather</div>;
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+// import useUser from "../hooks/useUser";
+
+export default function User() {
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/photos";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPhotos(data));
+  }, []);
+
+  return (
+    <div>
+      <div className="flex gap-2 flex-wrap">
+        {photos?.map((item, index) => (
+          <div key={index}>
+            <a href="#!" className="hover:underline">
+              <Image
+                src={item?.thumbnailUrl}
+                alt={item?.title}
+                width={200}
+                height={200}
+              />
+            </a>
+            <p>{item?.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
